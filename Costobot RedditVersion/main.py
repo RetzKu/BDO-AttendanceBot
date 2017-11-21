8fe1b9cd0ad79a61e8d42b1ee31e6b041724238b
RetzKu <retzkut@gmail.com>
2017-11-22 01:07:54 +0200

    basic commands surface built and bot loop.

--- Costobot RedditVersion/main.py
+++ Costobot RedditVersion/main.py
@@ -1,1 +1,44 @@
+import json
+import websockets
+import asyncio
+import discord
+import discord.ext.commands as discordbot
+import datetime
 
+#Own modules
+import src.commands
+
+
+class botclass:
+    def __init__(self):
+        with open('Keys.json') as fp:
+            keys = json.load(fp)
+        self.running = True
+        self.token = keys['Token']
+        self.discord = discordbot.Bot(command_prefix = '!')
+        self.excel = "exceli"
+        self.data_handler = "handlaaja"
+
+bot = botclass()
+
+@bot.discord.event
+async def on_ready():
+    await src.commands.add_commands(bot.discord,bot.excel,bot.data_handler)
+    print('Logged in as')
+    print(bot.discord.user.name)
+    print(bot.discord.user.id)
+    print('------')
+
+@bot.discord.event
+async def on_message(msg):
+    if isinstance(msg.channel, discord.abc.PrivateChannel):
+        print('private message received')
+    else:
+        await bot.discord.process_commands(msg)
+
+def run():
+   while bot.running is True:
+      bot.discord.loop.run_until_complete(bot.discord.start(bot.token))
+
+if __name__ == "__main__":
+    run()
--- Costobot RedditVersion/src/commands.py
+++ Costobot RedditVersion/src/commands.py
@@ -1,1 +1,13 @@
+from discord.ext.commands import *
+import discord
 
+def correct_assert(ctx) -> Context:
+    assert isinstance(ctx, Context)
+    return ctx
+
+async def add_commands(bot, excel, data_handler):
+
+    @bot.command(pass_context = True)
+    async def hello(ctx):
+        ctx = correct_assert(ctx) #Just fancy way to get visualstudio show insides of ctx
+        await ctx.channel.send("hi you :3")
